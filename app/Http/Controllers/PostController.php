d6e7ce73e1e406867c3102def58fbb4850912a79
Liman <limanmorina280@gmail.com>
2025-04-28 17:19:25 +0200

    Ndryshimet ne pjesen e validimeve,perdorimi try/catch,dhe shkurtesa e hapsirave

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,59 +4,58 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
 class PostController extends Controller
 {
-    
-   
-   public function deletePost(Post $post) {
-    if (auth()->user()->id === $post['user_id']) {
-       $post->delete();
-    }
-    return redirect('/');
-   }
-   
-   
-    public function actuallyUpdatePost(Post $post, Request $request) {
-        if (auth()->user()->id !== $post['user_id']) {
-            return redirect('/');
+    public function deletePost(Post $post)
+    {
+        if (auth()->user()->id === $post->user_id) {
+            $post->delete();
         }
-
-        $incomingFields = $request->validate([
-            'title' => 'required',
-            'body' => 'required'
-        ]);
-
-        $incomingFields['title'] = strip_tags($incomingFields['title']);
-        $incomingFields['body'] = strip_tags($incomingFields['body']);
-
-        $post->update($incomingFields);
         return redirect('/');
     }
-    
-    
-    
-    public function showEditScreen(Post $post) {
-        if (auth()->user()->id !== $post['user_id']) {
+
+    public function actuallyUpdatePost(Post $post, UpdatePostRequest $request)
+    {
+        if (auth()->user()->id !== $post->user_id) {
             return redirect('/');
         }
-        
-        return view('edit-post', ['post' => $post]);
 
+        try {
+            $post->update([
+                'title' => $request->title,
+                'body'  => $request->body,
+            ]);
+            return redirect('/');
+        } catch (Exception $e) {
+            return redirect('/')->with('error', 'Failed to update post.');
+        }
     }
-    
-    
-    public function createPost(Request $request) {
-          $incomingFields = $request->validate([
-              'title' => 'required',
-              'body'  => 'required'
-          ]);
 
-    $incomingFields['title'] = strip_tags($incomingFields['title']);
-    $incomingFields['body'] = strip_tags($incomingFields['body']);
-    $incomingFields['user_id'] = auth()->id();
-    Post::create($incomingFields);
-    return redirect('/');
+    public function showEditScreen(Post $post)
+    {
+        if (auth()->user()->id !== $post->user_id) {
+            return redirect('/');
+        }
 
+        return view('edit-post', ['post' => $post]);
+    }
+
+    public function createPost(StorePostRequest $request)
+    {
+        try {
+            Post::create([
+                'title' => $request->title,
+                'body'  => $request->body,
+                'user_id' => auth()->id(),
+            ]);
+            return redirect('/');
+        } catch (Exception $e) {
+            return redirect('/')->with('error', 'Failed to create post.');
+        }
     }
 }
