0aafd1176a22a3212696f62f5107625959bac1f9
Liman <limanmorina280@gmail.com>
2025-05-01 16:49:48 +0200

    Permirsim te kodit (shkaku i kllapes)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
             $post->update($data); 
     
             return redirect()->route('posts.index')->with('success', 'Its successful!');
-    }
+    
             return redirect('/');
         } catch (Exception $e) {
             return redirect('/')->with('error', 'Failed to update post.');
